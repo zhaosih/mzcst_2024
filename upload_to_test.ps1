1d19e182769ed0dd4189c2c80051e590428b7954
Mengzi Li <limengzi96@gmail.com>
2025-05-26 22:40:20 +0800

    Update upload_to_test.ps1

--- upload_to_test.ps1
+++ upload_to_test.ps1
@@ -8,5 +8,8 @@ python -m twine upload --repository testpypi dist/* --verbose
 # 删除本地打包文件
 Remove-Item dist/*
 
+# 延迟一段时间
+Start-Sleep -Seconds 60
+
 # 安装测试包
 pip install -i https://test.pypi.org/simple/ --upgrade mzcst-2024
